34d545630367c1bd083c92412a00e0a476620709
Ripcord Jeraldine <barutkin@gmail.com>
2019-11-28 14:48:43 +0300

    auto deploy: some fixes

--- roger-skyline-1.firstboot.sh
+++ roger-skyline-1.firstboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 20
+sleep 10
 firewall-cmd --permanent --remove-service='ssh'
 firewall-cmd --permanent --remove-service='dhcpv6-client'
 firewall-cmd --permanent --add-service='ssh-roger-skyline-1'
@@ -9,6 +9,7 @@ firewall-cmd --permanent --add-service='https'
 firewall-cmd --permanent --add-service='kibana'
 firewall-cmd --reload
 filebeat modules enable suricata apache system
+sleep 30
 filebeat setup -e
 newaliases
 
--- roger-skyline-1.post.sh
+++ roger-skyline-1.post.sh
@@ -1,86 +1,137 @@
 # Environment
+echo "LANG=en_US.utf-8 >> /etc/environment"
 echo "LANG=en_US.utf-8" >> /etc/environment
+echo "LC_ALL=en_US.utf-8 >> /etc/environment"
 echo "LC_ALL=en_US.utf-8" >> /etc/environment
 
 # VGA Resolution
 cp -v /etc/default/grub /etc/default/grub.backup
+echo "sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/' /etc/default/grub"
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/' /etc/default/grub
+echo "sed -i 's/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"vga=795 /' /etc/default/grub"
 sed -i 's/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"vga=795 /' /etc/default/grub
+echo "grub2-mkconfig -o /boot/grub2/grub.cfg"
 grub2-mkconfig -o /boot/grub2/grub.cfg
 
 # SSH
 cp -v /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
+echo "sed -i 's/#Port 22/Port 42122/' /etc/ssh/sshd_config"
 sed -i 's/#Port 22/Port 42122/' /etc/ssh/sshd_config
+echo "sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config"
 sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+echo "sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config"
 sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+echo "semanage port --add --type ssh_port_t --proto tcp 42122"
 semanage port --add --type ssh_port_t --proto tcp 42122
-cp /usr/lib/firewalld/services/ssh.xml /etc/firewalld/services/ssh-roger-skyline-1.xml
+cp -v /usr/lib/firewalld/services/ssh.xml /etc/firewalld/services/ssh-roger-skyline-1.xml
+echo "sed -i 's/port=\"22\"/port=\"42122\"/' /etc/firewalld/services/ssh-roger-skyline-1.xml"
 sed -i 's/port=\"22\"/port=\"42122\"/' /etc/firewalld/services/ssh-roger-skyline-1.xml
 mkdir -v -p /home/rjeraldi/.ssh
+echo "curl -o /home/rjeraldi/.ssh/authorized_keys https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/authorized_keys"
 curl -o /home/rjeraldi/.ssh/authorized_keys https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/authorized_keys
+echo "chown rjeraldi:rjeraldi -R /home/rjeraldi/.ssh"
 chown rjeraldi:rjeraldi -R /home/rjeraldi/.ssh
+echo "chmod 700 /home/rjeraldi/.ssh"
 chmod 700 /home/rjeraldi/.ssh
+echo "chmod 600 /home/rjeraldi/.ssh/id_rsa.pub"
 chmod 600 /home/rjeraldi/.ssh/id_rsa.pub
 
 # HTTPD
+echo "curl -o /etc/pki/tls/private/rjeraldi-roger-skyline-1.key https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/rjeraldi-roger-skyline-1.key"
 curl -o /etc/pki/tls/private/rjeraldi-roger-skyline-1.key https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/rjeraldi-roger-skyline-1.key
+echo "chmod 600 /etc/pki/tls/private/rjeraldi-roger-skyline-1.key"
 chmod 600 /etc/pki/tls/private/rjeraldi-roger-skyline-1.key
+echo "curl -o /etc/pki/tls/certs/rjeraldi-roger-skyline-1.crt https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/rjeraldi-roger-skyline-1.crt"
 curl -o /etc/pki/tls/certs/rjeraldi-roger-skyline-1.crt https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/rjeraldi-roger-skyline-1.crt
+echo "chmod 600 /etc/pki/tls/certs/rjeraldi-roger-skyline-1.crt"
 chmod 600 /etc/pki/tls/certs/rjeraldi-roger-skyline-1.crt
+echo "cp -v /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/ssl.conf.backup"
 cp -v /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/ssl.conf.backup
+echo "sed -i 's/SSLCertificateFile \/etc\/pki\/tls\/certs\/localhost.crt/SSLCertificateFile \/etc\/pki\/tls\/certs\/rjeraldi-roger-skyline-1.crt/' /etc/httpd/conf.d/ssl.conf"
 sed -i 's/SSLCertificateFile \/etc\/pki\/tls\/certs\/localhost.crt/SSLCertificateFile \/etc\/pki\/tls\/certs\/rjeraldi-roger-skyline-1.crt/' /etc/httpd/conf.d/ssl.conf
+echo "sed -i 's/SSLCertificateKeyFile \/etc\/pki\/tls\/private\/localhost.key/SSLCertificateKeyFile \/etc\/pki\/tls\/private\/rjeraldi-roger-skyline-1.key/' /etc/httpd/conf.d/ssl.conf"
 sed -i 's/SSLCertificateKeyFile \/etc\/pki\/tls\/private\/localhost.key/SSLCertificateKeyFile \/etc\/pki\/tls\/private\/rjeraldi-roger-skyline-1.key/' /etc/httpd/conf.d/ssl.conf
+echo "curl -o /etc/httpd/modsecurity.d/modsec.user.conf https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/modsec.user.conf"
 curl -o /etc/httpd/modsecurity.d/modsec.user.conf https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/modsec.user.conf
+echo "curl -o /etc/httpd/modsecurity.d/activated_rules/408.conf https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/408.conf"
 curl -o /etc/httpd/modsecurity.d/activated_rules/408.conf https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/408.conf
+echo "curl -O https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/html.tar.gz"
 curl -O https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/html.tar.gz
+echo "tar -xf html.tar.gz"
 tar -xf html.tar.gz
 mv -v html/* /var/www/html/
+echo "curl -O https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/mod_evasive-test.pl"
 curl -O https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/mod_evasive-test.pl
+echo "systemctl enable httpd"
 systemctl enable httpd
 
 # Suricata
+echo "yum --enablerepo=epel-testing install -y suricata"
 yum --enablerepo=epel-testing install -y suricata
 cp -v /etc/sysconfig/suricata /etc/sysconfig/suricata.backup
+echo "sed -i 's/OPTIONS=\"-i eth0/OPTIONS=\"-q 0/' /etc/sysconfig/suricata"
 sed -i 's/OPTIONS=\"-i eth0/OPTIONS=\"-q 0/' /etc/sysconfig/suricata
-cp -i /etc/crontab /etc/crontab.clean
-echo "@reboot root /usr/sbin/iptables -I INPUT -j NFQUEUE" >> /etc/crontab
-echo "@reboot root /usr/sbin/iptables -I OUTPUT -j NFQUEUE" >> /etc/crontab
+cp -v /etc/crontab /etc/crontab.clean
+echo "@reboot root sleep 30 && /usr/sbin/iptables -I INPUT -j NFQUEUE && /usr/sbin/iptables -I INPUT -j NFQUEUE >> /etc/crontab"
+echo "@reboot root sleep 30 && /usr/sbin/iptables -I INPUT -j NFQUEUE && /usr/sbin/iptables -I INPUT -j NFQUEUE" >> /etc/crontab
 mv -v /etc/suricata/suricata.yaml /etc/suricata/suricata.yaml.backup
+echo "curl -o /etc/suricata/suricata.yaml https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/suricata.yaml"
 curl -o /etc/suricata/suricata.yaml https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/suricata.yaml
+echo "chown suricata:suricata /etc/suricata/suricata.yaml"
 chown suricata:suricata /etc/suricata/suricata.yaml
+echo "chmod 640 /etc/suricata/suricata.yaml"
 chmod 640 /etc/suricata/suricata.yaml
+echo "curl -O https://rules.emergingthreats.net/open/suricata-`suricata -V | awk -F'This is Suricata version ' '{print $2}' | awk '{print $1}'`/emerging.rules.tar.gz"
 curl -O https://rules.emergingthreats.net/open/suricata-`suricata -V | awk -F'This is Suricata version ' '{print $2}' | awk '{print $1}'`/emerging.rules.tar.gz
+echo "tar -xf emerging.rules.tar.gz"
 tar -xf emerging.rules.tar.gz
-mkdir -p /var/lib/suricata/rules
-cp rules/emerging-scan.rules /var/lib/suricata/rules/emerging-scan-drop.rules
+mkdir -pv /var/lib/suricata/rules
+cp -v rules/emerging-scan.rules /var/lib/suricata/rules/emerging-scan-drop.rules
+echo "sed -i 's/^alert /drop /' /var/lib/suricata/rules/emerging-scan-drop.rules"
 sed -i 's/^alert /drop /' /var/lib/suricata/rules/emerging-scan-drop.rules
+echo "systemctl enable suricata"
 systemctl enable suricata
 
 # ELK
+echo "curl -o /etc/yum.repos.d/elastico.repo https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/elastico.repo"
 curl -o /etc/yum.repos.d/elastico.repo https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/elastico.repo
 cp -v /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.backup
+echo "sed -i 's/#network.host: 192.168.0.1/network.host: localhost/' /etc/elasticsearch/elasticsearch.yml"
 sed -i 's/#network.host: 192.168.0.1/network.host: localhost/' /etc/elasticsearch/elasticsearch.yml
 cp -v /etc/kibana/kibana.yml /etc/kibana/kibana.yml.backup
+echo "sed -i 's/#server.port: 5601/server.port: 5601/' /etc/kibana/kibana.yml"
 sed -i 's/#server.port: 5601/server.port: 5601/' /etc/kibana/kibana.yml
+echo "sed -i 's/#server.host: \"localhost\"/server.host: 0.0.0.0/' /etc/kibana/kibana.yml"
 sed -i 's/#server.host: \"localhost\"/server.host: 0.0.0.0/' /etc/kibana/kibana.yml
+echo "sed -i 's/#server.name: \"your-hostname\"/server.name: \"rjeraldi-roger-skyline-1\"/' /etc/kibana/kibana.yml"
 sed -i 's/#server.name: \"your-hostname\"/server.name: \"rjeraldi-roger-skyline-1\"/' /etc/kibana/kibana.yml
+echo "systemctl enable elasticsearch"
 systemctl enable elasticsearch
+echo "systemctl enable kibana"
 systemctl enable kibana
+echo "systemctl enable filebeat"
 systemctl enable filebeat
 
 # Update script
+echo "curl -O https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/update.sh"
 curl -O https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/update.sh
+echo "@reboot root /bin/bash /root/update.sh >> /etc/crontab"
 echo "@reboot root /bin/bash /root/update.sh" >> /etc/crontab
+echo "0 4 * * sun root /bin/bash /root/update.sh >> /etc/crontab"
 echo "0 4 * * sun root /bin/bash /root/update.sh" >> /etc/crontab
 cp -v /etc/aliases /etc/aliases.backup
+echo "root:           barutkin@gmail.com >> /etc/aliases"
 echo "root:           barutkin@gmail.com" >> /etc/aliases
 
 # Crontabwatch
+echo "curl -O https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/crontabwatch.sh"
 curl -O https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/crontabwatch.sh
+echo "@reboot root /bin/bash /root/crontabwatch.sh >> /etc/crontab"
 echo "@reboot root /bin/bash /root/crontabwatch.sh" >> /etc/crontab
 
 # First boot script
+echo "curl -o /root/roger-skyline-1.firstboot.sh https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/roger-skyline-1.firstboot.sh"
 curl -o /root/roger-skyline-1.firstboot.sh https://raw.githubusercontent.com/barutkin/roger-skyline-1/master/roger-skyline-1.firstboot.sh
-chmod +x /root/roger-skyline-1.firstboot.sh
-cp -i /etc/crontab /etc/crontab.backup
+cp -v /etc/crontab /etc/crontab.backup
+echo "@reboot root /bin/bash /root/roger-skyline-1.firstboot.sh > /root/roger-skyline-1.firstboot.log >> /etc/crontab"
 echo "@reboot root /bin/bash /root/roger-skyline-1.firstboot.sh > /root/roger-skyline-1.firstboot.log" >> /etc/crontab
